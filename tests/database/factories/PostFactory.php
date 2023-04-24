d3eaf6a23627aa3e4b3f66545c27fc1983371de9
Dominik <dominik@profi-webspace.com>
2023-04-24 10:40:51 +0200

    added mysql to actions

---
+++ tests/database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends Factory<User> */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(random_int(5, 15), true),
+            'created_at' => $this->faker->dateTimeBetween('-1 year'),
+        ];
+    }
+}
